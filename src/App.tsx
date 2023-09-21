6c616aa7bf39d6583c6ac52076cd8719c7b1a631
Vinicius Eduardo <vinibh0610@gmail.com>
2023-09-21 14:07:45 -0300

    Resolvendo conflitos com a branch principal

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,8 @@ import Login from './components/Login';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       <Login />
-=======
-    <div className="meals">
-      <span className="logo">Grupão</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
->>>>>>> main-group-23
     </div>
   );
 }
