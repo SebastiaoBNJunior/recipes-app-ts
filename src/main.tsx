ae43606bc12b89e5f5e66def8241f9c93711a39c
Vinicius Eduardo <vinibh0610@gmail.com>
2023-09-21 13:30:20 -0300

    Requisitos 02, 03, 05

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { RecipeProvider } from './context/search-results-context';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+  .render(
+    <BrowserRouter>
+      <RecipeProvider>
+        <App />
+      </RecipeProvider>
+    </BrowserRouter>,
+  );
