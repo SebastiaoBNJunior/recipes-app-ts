e5ac48110c425530f8ea0f79e5cda8f44d3f38e9
João Rafael <joaorafael2502@gmail.com>
2023-10-11 17:26:59 -0300

    request-41-done

--- src/pages/RecipeInProgress.tsx
+++ src/pages/RecipeInProgress.tsx
@@ -1,13 +1,42 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import RecipeDrinkInProgress from '../components/RecipeDrinkInProgress';
 import RecipeMealInProgress from '../components/RecipeMealInProgess';
 import ShareFavBtn from '../components/ShereFavBtn';
 import StartRecipeButton from '../components/StartRecipeButton';
+import { useRecipeContext } from '../context/search-results-context';
 
 function RecipeInProgress() {
   const location = useLocation();
   const { pathname } = location;
   console.log(pathname);
+
+  const { setRecipe } = useRecipeContext();
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        let endpoint = '';
+        if (window.location.pathname.includes('meals')) {
+          endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        } else if (window.location.pathname.includes('drinks')) {
+          endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+        }
+
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        setRecipe(data.meals ? data.meals[0] : data.drinks[0]);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro ao buscar detalhes da receita:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchRecipe();
+  }, [id]);
 
   return (
     <div>
